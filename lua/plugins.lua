dac1d19eb02d9d56b2830da4c14e8d050a9bfab2
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2023-03-26 01:57:43 -0500

    lazynvim to init.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 require('lazy').setup({
 
@@ -113,10 +95,5 @@ require('lazy').setup({
   'Olical/conjure',
   'wlangstroth/vim-racket',
 })
-
--- set color of side lines to white
-vim.cmd [[
-  hi VertSplit guifg=#ffffff
-]]
 
 require('Comment').setup()
