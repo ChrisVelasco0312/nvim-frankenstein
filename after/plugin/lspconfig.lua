443477497ef102891a6b45ba2ca30fc3813d4925
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-01-01 18:48:24 -0500

    [fix] tsserver file is not commonjs module diagnostics

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -3,7 +3,6 @@ local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-
 lspconfig.emmet_ls.setup({
   -- on_attach = on_attach,
   capabilities = capabilities,
@@ -45,3 +44,26 @@ vim.diagnostic.config({
     source = "always", -- Or "if_many"
   },
 })
+
+
+
+local function filter_tsserver_diagnostics(_, result, ctx, config)
+  if result.diagnostics == nil then
+    return
+  end
+  -- ignore some tsserver diagnostics
+  local idx = 1
+  while idx <= #result.diagnostics do
+    local entry = result.diagnostics[idx]
+    -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+    if entry.code == 80001 then
+      -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+      table.remove(result.diagnostics, idx)
+    else
+      idx = idx + 1
+    end
+  end
+  vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = filter_tsserver_diagnostics
