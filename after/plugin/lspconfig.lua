5985dbea7a39bab7a7296e0bdbd0937c1a588616
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-01-21 17:10:22 -0500

    Remove emmet ls config

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -3,29 +3,29 @@ local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-lspconfig.emmet_ls.setup({
-  -- on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'jsx' },
-  init_options = {
-    html = {
-      options = {
-        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-        ["bem.enabled"] = true,
-      },
-    },
-    jsx = {
-      options = {
-        ["jsx.enabled"] = true,
-        ["markup.attributes"] = {
-          ["class"] = "className",
-          ["for"] = "htmlFor",
-          ["tabindex"] = "tabIndex",
-        },
-      }
-    },
-  }
-})
+-- lspconfig.emmet_ls.setup({
+--   -- on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'jsx' },
+--   init_options = {
+--     html = {
+--       options = {
+--         -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+--         ["bem.enabled"] = true,
+--       },
+--     },
+--     jsx = {
+--       options = {
+--         ["jsx.enabled"] = true,
+--         ["markup.attributes"] = {
+--           ["class"] = "className",
+--           ["for"] = "htmlFor",
+--           ["tabindex"] = "tabIndex",
+--         },
+--       }
+--     },
+--   }
+-- })
 
 -- lspconfig.tsserver.setup({
 --   init_options = {
