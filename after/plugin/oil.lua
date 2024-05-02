7ecc16fee4e302384f0c1bf74e8df6490b014aec
Christian Velasco <Christian.Velasco@Outsourcing.libertyseguros.com>
2024-05-02 15:41:13 -0500

    add oil commands

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,3 +1,13 @@
 require("oil").setup({
   default_file_explorer = true,
+  float = {
+    max_width = 100,
+    max_height = 70,
+  }
 })
+
+vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+
+-- open in floating window
+vim.keymap.set("n", "<leader>o", require("oil").open_float, { desc = "Open parent directory in floating window" })
+
