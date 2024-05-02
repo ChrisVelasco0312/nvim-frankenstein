90551fb462f4b6b4e5c5894ffbf7325227fb8d5b
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-05-02 10:08:25 -0500

    add tabs for noneckpain

--- after/plugin/noneckpain.lua
+++ after/plugin/noneckpain.lua
@@ -1,7 +1,10 @@
 local neckPain = require("no-neck-pain")
 
 neckPain.setup({
+  killAllBuffersOnDisable = true,
+  width = 120,
   autocmds = {
     enableOnVimEnter = true,
+    enableOnTabEnter = true,
   }
 })
