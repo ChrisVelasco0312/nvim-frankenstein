48abe3e4aefe18f43f9cf6f53989e2694a43fee3
Christian Velasco <Christian.Velasco@Outsourcing.libertyseguros.com>
2024-05-08 18:07:04 -0500

    Better folding config

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -2,19 +2,19 @@ vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-
 local handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
-  local suffix = ('  %d '):format(endLnum - lnum)
+  local suffix = (' >> %d '):format(endLnum - lnum)
   local sufWidth = vim.fn.strdisplaywidth(suffix)
   local targetWidth = width - sufWidth
   local curWidth = 0
-  for _, chunk in ipairs(virtText) do
+  for _, chunk in pairs(virtText) do
     local chunkText = chunk[1]
     local chunkWidth = vim.fn.strdisplaywidth(chunkText)
     if targetWidth > curWidth + chunkWidth then
@@ -36,20 +36,23 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
   return newVirtText
 end
 
--- Option 3: treesitter as a main provider instead
--- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
--- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
 require('ufo').setup({
-  fold_virt_text_handler = handler,
-  preview = {
-    win_config = {
-      border = { '', '─', '', '', '', '─', '', '' },
-      winhighlight = 'Normal:Folded',
-      winblend = 0
-    }
-  },
-  provider_selector = function(bufnr, filetype, buftype)
-    return { 'treesitter', 'indent' }
-  end
+  fold_virt_text_handler = handler
 })
---
+
+-- Custom statscolumn
+local fcs = vim.opt.fillchars:get()
+
+-- Stolen from Akinsho
+local function get_fold(lnum)
+  if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then return ' ' end
+  local fold_sym = vim.fn.foldclosed(lnum) == -1 and fcs.foldopen or fcs.foldclose
+  return fold_sym
+end
+
+_G.get_statuscol = function()
+  return "%s%l " .. get_fold(vim.v.lnum) .. " "
+end
+
+
+vim.o.statuscolumn = "%!v:lua.get_statuscol()"
