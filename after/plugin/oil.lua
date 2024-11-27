0c4dc99894d369f435881d9edd9ec6e58f29544a
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-11-26 20:42:20 -0500

    map q to close oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -10,3 +10,6 @@ vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" }
 
 -- open in floating window
 vim.keymap.set("n", "sf", require("oil").open_float, { desc = "Open parent directory in floating window" })
+
+--close with q
+vim.keymap.set("n", "q", require("oil").close, { desc = "Close oil" })
