085984f05c2b373415fc00dd3d9d6fedd580b570
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2024-11-28 23:21:07 -0500

    add todo command

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -10,3 +10,6 @@ vim.api.nvim_set_keymap('n', '<leader>ld', '<cmd>TroubleToggle workspace_diagnos
 
 vim.api.nvim_set_keymap('n', '<leader>lo', '<cmd>TroubleToggle document_diagnostics<CR>',
   { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>td', '<cmd>TroubleToggle todo<CR>',
+  { noremap = true, silent = true, desc = '[T]rouble To[D]o' })
