4b3717b12bc2a49fba791f1b09c41e4d65f7b785
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2025-01-13 11:59:50 -0500

    replace codeium with github copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,4 +1,6 @@
 -- disable codeium by default
-vim.keymap.set('n', ';cc', ':CodeiumToggle<ENTER>', { silent = true, desc = 'Copilot Enable' })
-vim.keymap.set('n', ';ci', ':Codeium<ENTER>', { silent = true, desc = 'Copilot suggestions' })
-vim.keymap.set('i', '<C-,>', 'codeium#Accept("<CR>")', { expr = true, silent = true, desc = 'Accept Copilot suggestion' })
+vim.keymap.set('n', ';cc', ':Copilot enable<ENTER>', { silent = true, desc = 'Copilot Enable' })
+-- enable copilot
+vim.keymap.set('n', ';cd', ':Copilot disable<ENTER>', {
+  silent = true, desc = 'Copilot Disable'
+})
